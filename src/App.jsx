74623eccf7ea074e0a4df959b6b632a76113e6c0
unknown <kidus.muluwork@gmail.com>
2025-03-08 07:07:48 +0300

    initalized the page and added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='flex place-items-center flex-col mt-12'>
+      <div className='headerTexts'>
+        <h1  className='text-[#3fd673] text-4xl uppercase font-[Roboto] font-bold drop-shadow-2xl tracking-wider text-center pb-5'>leaderboard</h1>
+        <p className='flex text-center flex-col text-gray-500'><span>Code smarter, commit often, and climb the leaderboard, the more you push, the higher you rise!</span> Track your progress and lead the way in your innovation! 🚀</p>
+        </div>
+      <div className='Ranking mt-20'>
+        <div className='stages flex'>
+          <div></div>
+          <div></div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
